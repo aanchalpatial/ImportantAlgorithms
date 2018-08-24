53bdc09520764b42533eeda12c9dc7d96998f407
AP <apatial.27@gmail.com>
2018-08-24 21:53:37 +0530

    Add files via upload

---
+++ cpMicrosoft/imp_ds_vector_set_multiset_map_bitset_deque_stack_queue_priorityQueue.cpp
@@ -0,0 +1,104 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+   /* cout << "Hello World!" << endl;
+    return 0;
+    
+    
+    //------->>> data strucrures
+    
+    
+    vector<int> v={10,2,4,1,7};
+    cout<<v.back()<<"\t"<<v[2]<<"\n";
+    string s="aanchalpatial";
+    cout<<s.substr(5,5)<<"\n";
+    cout<<s.find('l')<<"\n";
+    
+    
+    set<int> m;
+    m.insert(9);
+    m.insert(1);
+    m.insert(3);
+    m.insert(2);
+    m.insert(2);
+    // not possible for sets .....cout<<m[2];
+    //set maintains order
+    //all elements are distinct
+    cout<<m.size()<<"\t"<<m.count(2)<<"\n";
+    //cant use random access operator [] in set
+    //acessing nth element using advance
+    set<int>::iterator itr=m.begin();               //shorter way auto itr=m.begin();
+    advance(itr, 3);
+    cout<<"3rd element of set is: "<<*itr<<"\n";
+    
+    
+    
+    multiset<int> mt;
+    //same as sets but can save multiple occurences of elements
+    mt.insert(9);
+    mt.insert(1);
+    mt.insert(3);
+    mt.insert(2);
+    mt.insert(2);
+    cout<<mt.count(2)<<"\n";
+    
+    
+    
+    map<string,int> mp;				//by default sorts acc to key
+    //contains key-value pairs
+    mp["himachal"]=177401;
+    mp["delhi"]=110037;
+    cout<<mp["delhi"]<<"\t"<<mp["assam"];//<<"\t"<<mp.first;
+    //prints all the keys and values in a map:
+    for (auto x : mp) {
+    cout << x.first << " " << x.second << "\n";}
+    
+    
+    
+    bitset<5>bt(string("10111"));               //from right to left
+    cout<<bt[3]<<"\t"<<bt.count()<<"\n";
+   
+    
+    deque<int> dq;
+    //queue that can be changed at both ends of the array
+    dq.push_back(1);
+    dq.push_front(2);
+    dq.push_back(4);
+    dq.push_back(6);
+    for(auto x:dq)
+    cout<<x<<"\t";
+    cout<<"\n";
+    
+    
+    stack<int> st;
+    st.push(1);
+    st.push(4);
+    cout<<st.top()<<"\n";
+    
+    
+    queue<int> s;
+    s.push(3);
+    s.push(2);
+    s.push(5);
+    cout << s.front(); // 3
+    s.pop();
+    cout << s.front(); // 2
+    
+    
+    priority_queue<int> pq;                     //by default sorted in decreasing order
+    pq.push(2);
+    pq.push(5);
+    pq.push(1);
+    cout<<pq.top()<<"\n";                       //shows highest priority element
+    pq.pop();                                   //removes highest priority element
+    cout<<pq.top()<<"\n";
+    */
+    
+    
+    
+    
+    
+    
+}
